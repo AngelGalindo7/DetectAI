b9aa2adff99130b6788fc0270b64be2252a7ee8f
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-05-27 21:48:05 -0700

    Bug fix

--- frontend/sound-ethics-capstone/src/components/LoadingPage.jsx
+++ frontend/sound-ethics-capstone/src/components/LoadingPage.jsx
@@ -75,7 +75,7 @@ export default function LoadingPage() {
         setUploadStatus("Connection lost. Still trying...");
       }
     }
-  }, 500);
+  }, 200);
 
   intervalRef.current = interval;
 };
